20960b8c8f721fd03a777d806f28248fb00c7724
Houda Jawad <houdajawad@gmail.com>
2023-03-05 20:31:12 -0800

    Add a function to update winner and a method to the game class to reset game

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
 class Game {
  constructor() {
-    this.board = new Array(9).fill(null) //Data model
+    this.board = new Array(9).fill(null) 
     this.player1 = new Player("first", "💖")
     this.player2 = new Player("second", "🦄") 
     this.currentPlayer = this.player1.token
+    this.winningPlayer = this.player1.wins
+    this.winner = null
 }
 
   
@@ -16,12 +18,74 @@ if (this.currentPlayer === this.player1.token) {
 }
 
 makeAMove(i) {
+
+if(this.endGame()) {
+    this.resetGame()
+    return 
+}
+
 if (this.board[i]) {
     return
 }
-this.board[i] = this.currentPlayer
+console.log(this.board[i] = this.currentPlayer)
+var winningCombo = this.checkForWin()
+
+if(!winningCombo) {
+this.nextTurn()
 }
+
+
 }
+
+
+checkForWin() {
+    var winningPositions = [
+       [0, 1, 2],
+       [3, 4, 5],
+       [6, 7, 8],
+       [0, 3, 6],
+       [1, 4, 7],
+       [2, 5, 8],
+       [0, 4, 8],
+       [2, 4, 6]
+     ]
+     for (var i = 0; i < winningPositions.length; i++) {
+       var [a, b, c] = winningPositions[i]
+      if (this.board[a] && this.board[a] === this.board[b] && this.board[a] === this.board[c]) {
+         return winningPositions[i]        
+       }  
+     }
+     return null
+   }
+
+   endGame() {
+    var winningCombo = this.checkForWin()
+    if (winningCombo) {
+        return true 
+    } else {
+        return false
+    }
+
+   }
+
+   resetGame() {{
+     this.board = new Array(9).fill(null) 
+     this.currentPlayer = this.player1.token  
+    }
+    
+
+   }
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 // query Selectors
 var boxes = document.querySelectorAll(".box")
-console.log(boxes)
 var playerTurn = document.getElementById("playerTurn")
+var firstPlayerWins = document.querySelector(".heart-wins")
+var secondPlayerWins = document.querySelector(".unicorn-wins")
+
 
 // golobal variables
 var game = new Game()
-
+console.log(game.board)
 
 
 // // event listeners
@@ -20,7 +22,7 @@ boxes.forEach(box => {
 function onClick(i) {
 game.makeAMove(i)
 updateboard()
-game.nextTurn()
+updateWins()
 
 }
 
@@ -28,8 +30,41 @@ function updateboard() {
     for (let i = 0; i < game.board.length; i++) {
      var boxes = document.querySelector(`.box[data-index='${i}']`)
      boxes.innerText = game.board[i]
-  }
-}
+     playerTurn.innerText = `It's ${game.currentPlayer} turn`
+    
+   if (game.endGame()) {
+    playerTurn.innerText = `${game.currentPlayer} Won!` 
+    } else if (game.board[i]) {
+        playerTurn.innerText = `it's a draw!`   
+       }
+    } 
+       
+    } 
+
+
+   function updateWins() {
+        var winningCombo = game.checkForWin()
+        if (winningCombo && game.currentPlayer === "💖") {
+            game.player1.increaseWins()
+            firstPlayerWins.innerText = `${game.player1.wins} Wins` 
+        } else if (winningCombo && game.currentPlayer === "🦄")
+           game.player2.increaseWins()
+           secondPlayerWins.innerText = `${game.player2.wins} Wins`
+    }
+
+
+
+
+
+   
+
+    
+
+
+
+
+
+
 
 
 
